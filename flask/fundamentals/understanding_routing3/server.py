bbe2c662d49cfb557e776202e700fb4f9e35c118
Mitchell Esparza <Alexis75k@icloud.com>
2023-05-25 17:07:03 -0700

    I was fixing an install error with pip and flask

--- flask/fundamentals/understanding_routing3/server.py
+++ flask/fundamentals/understanding_routing3/server.py
@@ -4,7 +4,6 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
 @app.route('/')          # The "@" decorator associates this route with the function immediately following, which is hello_world()
 def hello_world():
-    print (hello_world)
     return "Hello World!"  # Return the string 'Hello World!' as a response
 
 @app.route('/dojo')
