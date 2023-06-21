cbbb460dedd1ba8913e0d8e862ca6a8874b30808
Mitchell Esparza <Alexis75k@icloud.com>
2023-06-21 13:40:07 -0700

    updates

--- flask_mysql/burgerz/flask_app/models/burger.py
+++ flask_mysql/burgerz/flask_app/models/burger.py
@@ -15,13 +15,13 @@ class Burger:
     @classmethod
     def save(cls,data):
         query = "INSERT INTO burgers (name,bun,meat,calories,created_at,updated_at) VALUES (%(name)s,%(bun)s,%(meat)s,%(calories)s,NOW(),NOW())"
-        return connectToMySQL('login_reg').query_db(query,data) #the 'burgers' is the name of the database in mysql
+        return connectToMySQL('burgerz').query_db(query,data) #the 'burgers' is the name of the database in mysql
 
 # gets all the burgers and returns them in a list of burger objects .
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM burgers;"
-        burgers_from_db =  connectToMySQL('login_reg').query_db(query)
+        burgers_from_db =  connectToMySQL('burgerz').query_db(query)
         burgers =[]
         for b in burgers_from_db: # type: ignore
             burgers.append(cls(b))
@@ -30,19 +30,19 @@ class Burger:
     @classmethod
     def get_one(cls,data):
         query = "SELECT * FROM burgers WHERE burgers.id = %(id)s;"
-        burger_from_db = connectToMySQL('login_reg').query_db(query,data)
+        burger_from_db = connectToMySQL('burgerz').query_db(query,data)
 
         return cls(burger_from_db[0]) # type: ignore
 
     @classmethod
     def update(cls,data):
         query = "UPDATE burgers SET name=%(name)s, bun=%(bun)s, meat=%(meat)s, calories=%(calories)s,updated_at = NOW() WHERE id = %(id)s;"
-        return connectToMySQL('login_reg').query_db(query,data)
+        return connectToMySQL('burgerz').query_db(query,data)
 
     @classmethod
     def destroy(cls,data):
         query = "DELETE FROM burgers WHERE id = %(id)s;"
-        return connectToMySQL('login_reg').query_db(query,data)
+        return connectToMySQL('burgerz').query_db(query,data)
     
 
     # Other Burger methods remain up yonder.
---
+++ flask_mysql/validation/login_reg_assignment/server.py
@@ -0,0 +1,2 @@
+#the proper date format to put a birthdate in mysql is YYYY-MM-DD (year-month-day)
+#the data type for a password in mysql is varchar(255)
